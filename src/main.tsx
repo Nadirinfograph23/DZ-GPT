198e6ad1ffbb11e88a3acc7e169da28057bf466b
Nadirinfograph23 <nadirfortest44@gmail.com>
2026-04-13 11:44:40 +0100

    Remove AI Chat DZ - redirect all routes to DZ Agent only

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
 import DZAgent from './pages/DZAgent.tsx'
-import ChatPage from './pages/ChatPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<Navigate to="/dz-agent" replace />} />
         <Route path="/dz-agent" element={<DZAgent />} />
-        <Route path="/chat" element={<ChatPage />} />
+        <Route path="*" element={<Navigate to="/dz-agent" replace />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,

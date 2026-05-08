797924da88fb707885e1eb3ced832d67ff87805d
Nadirinfograph23 <nadirfortest44@gmail.com>
2026-05-08 01:38:25 +0100

    feat: add /agent-v5 route

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import AIQuran from './pages/AIQuran.tsx'
 import DZChat from './pages/DZChat.tsx'
 import DZTube from './pages/DZTube.tsx'
 import DesignIntelligence from './pages/DesignIntelligence.tsx'
+import DZAgentV5 from './pages/DZAgentV5.tsx'
 import { MiniPlayerProvider } from './context/MiniPlayerContext.tsx'
 import MiniPlayer from './components/MiniPlayer.tsx'
 
@@ -26,6 +27,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/dz-tube" element={<DZTube />} />
           <Route path="/dztube" element={<Navigate to="/dz-tube" replace />} />
           <Route path="/design" element={<DesignIntelligence />} />
+          <Route path="/agent-v5" element={<DZAgentV5 />} />
           <Route path="/aiquran" element={<Navigate to="/quran" replace />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>

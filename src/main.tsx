f295d3d9ed74f3ada1391fec4e05089586e5af2a
Nadirinfograph23 <nadirfortest44@gmail.com>
2026-05-26 02:45:25 +0100

    feat: add /github-agent route

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ import DZTools from './pages/DZTools.tsx'
 import DZWebBuilder from './pages/DZWebBuilder.tsx'
 import OCRDZ from './pages/OCRDZ.tsx'
 import DZLe3ba from './pages/DZLe3ba.tsx'
+import DZAgentGitHub from './pages/DZAgentGitHub.tsx'
 import { MiniPlayerProvider } from './context/MiniPlayerContext.tsx'
 import MiniPlayer from './components/MiniPlayer.tsx'
 import QuickNav from './components/QuickNav.tsx'
@@ -73,6 +74,7 @@ createRoot(document.getElementById('root')!).render(
             <Route path="/web-builder" element={<DZWebBuilder />} />
             <Route path="/ocr-dz" element={<OCRDZ />} />
             <Route path="/le3ba" element={<DZLe3ba />} />
+            <Route path="/github-agent" element={<DZAgentGitHub />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
           <ConditionalMiniPlayer />
